9f1f371b6a43fa1328b2e23a390f096948980c11
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-06 14:02:16 -0300

    Criar método construtor da classe Fatura

--- src/br/com/zup/Fatura.java
+++ src/br/com/zup/Fatura.java
@@ -5,4 +5,9 @@ public class Fatura {
     private double valorDaFatura;
     private String dataDeVencimento;
 
+    public Fatura(Consumidor consumidor, double valorDaFatura, String dataDeVencimento) {
+        this.consumidor = consumidor;
+        this.valorDaFatura = valorDaFatura;
+        this.dataDeVencimento = dataDeVencimento;
+    }
 }
