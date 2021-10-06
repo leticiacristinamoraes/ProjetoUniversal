86ab025ca493c40b4a3bf32f7f3c4f25dafd4cb5
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-06 14:28:03 -0300

    Criar menu na classe sistema

--- src/br/com/zup/ServiceFatura.java
+++ src/br/com/zup/ServiceFatura.java
@@ -5,4 +5,5 @@ import java.util.List;
 
 public class ServiceFatura {
     private static List<Fatura> faturas = new ArrayList<>();
+
 }
--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,10 @@
 package br.com.zup;
 
 public class Sistema {
+    public static void menu(){
+        System.out.println("Bem vinde a Space Negócios *-*");
+        System.out.println("Digite 1 para cadastrar um novo consumidor.");
+        System.out.println("Digite 2 para cadastrar uma nova fatura.");
+        System.out.println("Digite 3 para sair do sistema.");
+    }
 }
