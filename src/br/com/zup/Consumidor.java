f4a2c265e561262acb8d4bd95ccd4931af6e6e56
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-06 13:57:42 -0300

    Criar métodos da classe consumidor

--- src/br/com/zup/Consumidor.java
+++ src/br/com/zup/Consumidor.java
@@ -3,4 +3,29 @@ package br.com.zup;
 public class Consumidor {
     private String nome;
     private String email;
+
+    public Consumidor(){
+
+    }
+
+    public Consumidor(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
