90cca33bec7db7b081cc6cdcc1541aa966d320e3
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-06 14:35:21 -0300

    Criar método para cadastrar um consumidor

--- src/br/com/zup/ServiceConsumidor.java
+++ src/br/com/zup/ServiceConsumidor.java
@@ -5,4 +5,10 @@ import java.util.List;
 
 public class ServiceConsumidor {
     private static List<Consumidor> consumidores = new ArrayList<>();
+
+    public static Consumidor cadastrarConsumidor(String nome, String email) {
+        Consumidor consumidor = new Consumidor(nome, email);
+        consumidores.add(consumidor);
+        return consumidor;
+    }
 }
