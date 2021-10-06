3fc619ca184a892b5a4f9e4afe70a6ad0fa63253
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-10-06 14:23:21 -0300

    Criar Lista de consumidores

--- src/br/com/zup/ServiceConsumidor.java
+++ src/br/com/zup/ServiceConsumidor.java
@@ -1,4 +1,8 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceConsumidor {
+    private static List<Consumidor> consumidores = new ArrayList<>();
 }
